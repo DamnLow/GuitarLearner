29239b1b9f4c80af9b27bbd4719e39b5bd708c55
Kiet <hoang.kiet.ta@gmail.com>
2020-05-24 17:13:33 +0200

    Learning Screen first implementation

---
+++ app/src/main/java/container/LearningScreen/LearningScreen.java
@@ -0,0 +1,17 @@
+package container.LearningScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Switch;
+
+import com.example.guitarlearner.R;
+
+public class LearningScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learning_screen);
+    }
+}
